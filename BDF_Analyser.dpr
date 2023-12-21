c12251ac1d129bd39c20fa0c4f0e274d88899710
bruno185 <salut.lespotes@gmail.com>
2023-12-22 00:33:08 +0100

    c

---
+++ BDF_Analyser.dpr
@@ -0,0 +1,15 @@
+program BDF_Analyser;
+
+uses
+  Vcl.Forms,
+  Unit1 in 'Unit1.pas' {Form1},
+  MyUtils in 'MyUtils.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
